8e15c3d748d75bd209b225fdfce7479abdb8e212
Michael Scott Hertzberg <mshertzberg@gmail.com>
2015-12-26 23:02:16 -0500

    correct import for Promise from `native-or-bluebird`

--- src/core/fetch.js
+++ src/core/fetch.js
@@ -1,6 +1,6 @@
+import Promise from 'native-or-bluebird'
 import { readFileSync } from 'fs'
 import { hasDOM } from 'hasdom'
-import { Promise } from 'native-or-bluebird'
 import { OAuth } from 'mashape-oauth'
 import { apiConfig } from './config'
 import { CACHE,
